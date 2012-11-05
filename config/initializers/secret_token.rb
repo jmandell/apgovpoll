4b6ff3a4aa3ce39b653bcdcd6b7b67b9cdc5d09a
Justin Griggs <LMHS-JGriggs@apps.mywccc.org>
2012-11-04 21:40:15 -0500

    Initial Rails Code

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Apgovpoll::Application.config.secret_token = 'a008a84e8ca91627956155581f77a3e45da5c5a7c3473769289f12d082b3c47b5b0bf17684ddabaf368dd784734e59ab92be98e423b1cb00f3ef84b42fae86e5'
